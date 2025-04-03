4febf743538ab130480dd0a371fed488b5bb6e17
Jambr0li <jasonkovalenko@gmail.com>
2025-04-03 09:52:45 -0700

    Initial Commit

---
+++ Raccon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Raccon
+//
+//  Created by Jason Kovalenko on 4/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raccon/RacconApp.swift
@@ -0,0 +1,17 @@
+//
+//  RacconApp.swift
+//  Raccon
+//
+//  Created by Jason Kovalenko on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RacconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
